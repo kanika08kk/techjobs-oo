798e9df59bbf1dc19335ae57266696102c6da5a6
Kiran Kapoor <kapoorkiran@gmail.com>
2017-12-14 16:33:00 -0600

    Final techjobs-OO

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.launchcode.models.Job;
 
 import javax.validation.Valid;
 
@@ -24,7 +25,7 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        model.addAttribute("job", jobData.findById(id));
         return "job-detail";
     }
 
@@ -40,8 +41,24 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        if (errors.hasErrors()) {
+            return "new-job";
+        }
+        else {
+            Job newJob = new Job(
+                    jobForm.getName(),
+                    jobData.getEmployers().findById(jobForm.getEmployerId()),
+                    jobData.getLocations().findById(jobForm.getLocationId()),
+                    jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
+                    jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId())
+            );
+            jobData.add(newJob);
 
-        return "";
+            model.addAttribute("job", newJob);
+
+            return "redirect:/job?id=" + newJob.getId();
+        }
+
 
     }
 }
